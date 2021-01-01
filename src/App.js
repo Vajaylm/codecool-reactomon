4da4fe88df1ec05ce99dd683361fc77a02df3736
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-01-01 15:03:48 +0100

    Prepare routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <h1>App</h1>
-      </div>
+      <Router>
+        <div>
+          <Navbar />
+          <Route exact path="/" render={props => (
+            <h1>App</h1>
+          )} />
+          <Route path="/pokemons" render={props => (
+            <h1>Pokemons</h1>
+          )} />
+          <Route path="/types" render={props => (
+            <h1>Types</h1>
+          )} />
+        </div>
+      </Router>
     );
   }
 }
