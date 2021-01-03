3bab495bd2098ecc770792d6f737cf90716fcad6
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-01-03 10:53:51 +0100

    Capitalize pokemon and type names

--- src/components/content/Pokemon.js
+++ src/components/content/Pokemon.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import Util from '../utility/Util';
 
 class Pokemon extends Component {
   render() {
     const { id, name } = this.props.pokemon;
     return (
       <div>
-        <Link to={ '/pokemon/' + id }>{ name }<br /></Link>
+        <Link to={ '/pokemon/' + id }>{ Util.capitalize(name) }<br /></Link>
       </div>
     )
   }
--- src/components/content/PokemonType.js
+++ src/components/content/PokemonType.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Util from '../utility/Util';
 
 class PokemonType extends Component {
   render() {
     const {name} = this.props.type;
     return (
       <div>
-        <p>{ name }</p>
+        <p>{ Util.capitalize(name) }</p>
       </div>
     )
   }
