c0c63e77b3c8b5c245fc7e820cbfc2d6d5a02392
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-01-03 18:40:54 +0100

    Use hooks for pokemon details, rework

--- src/components/content/pokemonDetails/PokemonDetail.js
+++ src/components/content/pokemonDetails/PokemonDetail.js
@@ -1,39 +1,33 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import Util from '../../utility/Util';
+import { capitalize } from '../../utility/Util';
 import AdvancedDetails from './AdvancedDetails';
+import { useRouteMatch } from "react-router-dom";
 
-class PokemonDetail extends Component {
-  state = {
-    details: []
-  }
+const PokemonDetail = props => {
+  const [details, setDetails] = useState([]);
+  const pokemonId = useRouteMatch("/pokemon/:pokemonId").params.pokemonId;
+  console.log(pokemonId);
+
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/' + pokemonId)
+      .then(response => { setDetails(response.data) });
+  }, [pokemonId]);
   
-  componentDidMount() {
-    const pokemon = this.props.pokemon[0];
-    const id = Util.getIdFromUrl(pokemon.url);
-    
-    axios.get('https://pokeapi.co/api/v2/pokemon/' + id)
-      .then(response => ( 
-        this.setState({ details: response.data })
-      ));
-  }
+  const { name, height, weight, stats } = details;
   
-  render() {
-    const { name, id, height, weight, stats } = this.state.details;
-    
-    if (stats !== undefined) {
-      return (
-        <div>
-          <h1>Name: { Util.capitalize(name) }</h1>
-          <h3>ID: { id }</h3>
-          <h3>Height: { height }</h3>
-          <h3>Weight: { weight }</h3>
-          <AdvancedDetails details={ this.state.details } />
-        </div>
-      );
-    }
-    else { return null; }
+  if (stats !== undefined) {
+    return (
+      <div>
+        <h1>Name: { capitalize(name) }</h1>
+        <h3>ID: { pokemonId }</h3>
+        <h3>Height: { height }</h3>
+        <h3>Weight: { weight }</h3>
+        <AdvancedDetails details={ details } />
+      </div>
+    );
   }
+  else { return null; }
 }
 
 export default PokemonDetail;
