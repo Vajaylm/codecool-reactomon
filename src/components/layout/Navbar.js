97171db002b9f2f2e61b203c62bbc4f9c8243c24
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-01-03 18:40:05 +0100

    Use hooks in Navbar

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Navbar() {
+const Navbar = props => {
   return (
     <header style={navbarStyle}>
       <h1>Pokemon</h1>
