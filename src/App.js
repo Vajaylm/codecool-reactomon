fd9815174c84131ccfff846d4d7565dc1f1472ef
Laszlo Miklos Vajay <vajaylm@gmail.com>
2021-01-05 19:41:29 +0100

    Separate and style header and content div

--- src/App.js
+++ src/App.js
@@ -1,30 +1,44 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
-
+import styled from 'styled-components';
 import Navbar from './components/layout/Navbar';
 import PokemonList from './components/content/PokemonList';
 import TypeList from './components/content/TypeList';
 import PokemonDetail from './components/content/pokemonDetails/PokemonDetail';
 import './App.css';
 
+const NavigationDiv = styled.div`
+  width: 80%;
+  font-family: Marker Felt, fantasy;
+  transform: translate(10%, 0px);
+`
+
+const ContentDiv = styled.div`
+  width: 80%;
+  font-family: Marker Felt, fantasy;
+  transform: translate(10%, 0px);
+`
+
 const App = props => {
   const content = (
     <Router>
-      <div>
+      <NavigationDiv>
         <Navbar />
+      </NavigationDiv>
+      <ContentDiv>
         <Route exact path="/">
           <Redirect to="/pokemons" />
         </Route> 
         <Route path="/pokemons" render={props => (
           <PokemonList />
-        )} />
+          )} />
         <Route path="/types" render={props => (
           <TypeList />
-        )} />
+          )} />
         <Route path="/pokemon/:pokemonId" render={props => (
           <PokemonDetail />
-        )} />
-      </div>
+          )} />
+      </ContentDiv>
     </Router>
   );
 
